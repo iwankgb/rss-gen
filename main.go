5454188cd745fbe8d6ae48ac0bfb5ec13db3f99d
Maciej Iwanowski <maciej.iwanowski@critical.today>
2015-08-15 19:40:51 +0200

    correct file handling, dates map generated as expected

--- main.go
+++ main.go
@@ -14,7 +14,8 @@ import "fmt"
 const worldReadable = 0644
 
 var yamlFilePath = flag.String("yaml", "", "Path to yaml file")
-var existingRssFilePath = flag.String("rss-existing", "", "Path to RSS file to be updated")
+var existingRssFilePath = flag.String("rss", "", "Path to RSS file to be updated")
+var itemsCount = flag.Int("count", 10, "Number of items to be included in the feed")
 
 func main() {
 	flag.Parse()
@@ -22,18 +23,27 @@ func main() {
 		fmt.Errorf("You have to provide -yaml parameter!")
 		panic("No yaml file path specified")
 	}
+	if *existingRssFilePath == "" {
+		fmt.Errorf("You have to provide -rss parameter!")
+		panic("No RSS file path specified")
+	}
 	existingRss := new(rss.Channel)
-	if *existingRssFilePath != "" {
-		rssFile, _ := os.Open(*existingRssFilePath)
-		defer rssFile.Close()
-		rssFileInfo, _ := rssFile.Stat()
-		rssContent := make([]byte, rssFileInfo.Size())
+	rssFile, _ := os.Open(*existingRssFilePath)
+	defer rssFile.Close()
+	rssFileInfo, isRssFileValid := rssFile.Stat()
+	fmt.Printf("%+v\n", isRssFileValid)
+	if isRssFileValid == nil {
+		rssFileSize := rssFileInfo.Size()
+		rssContent := make([]byte, rssFileSize)
 		rssReader := bufio.NewReader(rssFile)
 		rssReader.Read(rssContent)
-		existingRss := new(rss.Channel)
+		existingRss = new(rss.Channel)
 		xml.Unmarshal(rssContent, existingRss)
+		fmt.Printf("%+v\n", existingRss)
 	}
+	fmt.Printf("%+v\n", existingRss.Items)
 	datesMap := rss.NewDates(existingRss)
+	fmt.Printf("%+v\n", datesMap)
 	yamlFile, _ := os.Open(*yamlFilePath)
 	defer yamlFile.Close()
 	yamlFileInfo, _ := yamlFile.Stat()
@@ -42,8 +52,8 @@ func main() {
 	yamlReader.Read(yamlContent)
 	var yamlObject yaml.Channel
 	y.Unmarshal(yamlContent, &yamlObject)
-	rssObject := rss.BuildRssFromYaml(yamlObject, 10, datesMap)
+	rssObject := rss.BuildRssFromYaml(yamlObject, *itemsCount, datesMap)
 	//	fmt.Printf("%+v", rssObject)
 	rssXml, _ := xml.Marshal(&rssObject)
-	ioutil.WriteFile("/tmp/test.xml", rssXml, os.ModeExclusive)
+	ioutil.WriteFile(*existingRssFilePath, rssXml, 0644)
 }
