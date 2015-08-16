9240dd8c4cdfa36b3cd7b5d0d69c09fec3c0897d
Maciej Iwanowski <maciej.iwanowski@critical.today>
2015-08-16 02:04:09 +0200

    trying to make main() as short as possible

--- main.go
+++ main.go
@@ -2,54 +2,93 @@
 package main
 
 import y "gopkg.in/yaml.v2"
-import "flag"
-import "os"
-import "bufio"
-import "github.com/iwankgb/rss-gen/yaml"
-import "github.com/iwankgb/rss-gen/rss"
-import "encoding/xml"
-import "io/ioutil"
-import "fmt"
+import (
+	"bufio"
+	"encoding/xml"
+	"flag"
+	"fmt"
+	"github.com/iwankgb/rss-gen/rss"
+	"github.com/iwankgb/rss-gen/yaml"
+	"io/ioutil"
+	"os"
+)
 
-const worldReadable = 0644
+type arguments struct {
+	yamlFilePath *string
+	rssFilePath  *string
+	itemsCount   *int
+}
 
-var yamlFilePath = flag.String("yaml", "", "Path to yaml file")
-var existingRssFilePath = flag.String("rss", "", "Path to RSS file to be updated")
-var itemsCount = flag.Int("count", 10, "Number of items to be included in the feed")
+var args = arguments{
+	flag.String("yaml", "", "Path to yaml file"),
+	flag.String("rss", "", "Path to RSS file to be updated"),
+	flag.Int("count", 10, "Number of items to be included in the feed"),
+}
 
 func main() {
 	flag.Parse()
-	if *yamlFilePath == "" {
+	validateArguments(&args)
+	rssFile, _ := os.Open(*args.rssFilePath)
+	defer rssFile.Close()
+	existingRss := prepareExistingRssChannel(rssFile)
+	dates := rss.NewDates(existingRss)
+	yamlFile, _ := os.Open(*args.yamlFilePath)
+	defer yamlFile.Close()
+	yamlObject := prepareYaml(yamlFile)
+	rssXml := prepareRss(yamlObject, &args, dates)
+	ioutil.WriteFile(*args.rssFilePath, rssXml, 0644)
+	fmt.Println("RSS updated successfully")
+}
+
+func validateArguments(argument *arguments) {
+	if *argument.yamlFilePath == "" {
 		fmt.Println("You have to provide -yaml parameter!")
 		os.Exit(1)
 	}
-	if *existingRssFilePath == "" {
+	if *argument.rssFilePath == "" {
 		fmt.Println("You have to provide -rss parameter!")
 		os.Exit(2)
 	}
+}
+
+func prepareExistingRssChannel(rssFile *os.File) *rss.Channel {
+	_, isRssFileValid := rssFile.Stat()
 	existingRss := new(rss.Channel)
-	rssFile, _ := os.Open(*existingRssFilePath)
-	defer rssFile.Close()
-	rssFileInfo, isRssFileValid := rssFile.Stat()
 	if isRssFileValid == nil {
-		rssFileSize := rssFileInfo.Size()
-		rssContent := make([]byte, rssFileSize)
-		rssReader := bufio.NewReader(rssFile)
-		rssReader.Read(rssContent)
+		rssContent := readFile(rssFile)
 		existingRss = new(rss.Channel)
 		xml.Unmarshal(rssContent, existingRss)
 	}
-	datesMap := rss.NewDates(existingRss)
-	yamlFile, _ := os.Open(*yamlFilePath)
-	defer yamlFile.Close()
-	yamlFileInfo, _ := yamlFile.Stat()
-	var yamlContent = make([]byte, yamlFileInfo.Size())
-	yamlReader := bufio.NewReader(yamlFile)
-	yamlReader.Read(yamlContent)
-	var yamlObject yaml.Channel
-	y.Unmarshal(yamlContent, &yamlObject)
-	rssObject := rss.BuildRssFromYaml(yamlObject, *itemsCount, datesMap)
+	return existingRss
+}
+
+func readFile(file *os.File) []byte {
+	fileInfo, _ := file.Stat()
+	var fileContent = make([]byte, fileInfo.Size())
+	fileReader := bufio.NewReader(file)
+	fileReader.Read(fileContent)
+	return fileContent
+}
+
+func prepareYaml(file *os.File) *yaml.Channel {
+	yamlContent := readFile(file)
+	return getYamlObject(&yamlContent)
+}
+
+func getYamlObject(yamlContent *[]byte) *yaml.Channel {
+	yamlObject := new(yaml.Channel)
+	y.Unmarshal(*yamlContent, yamlObject)
+	return yamlObject
+}
+
+func prepareRss(yamlObject *yaml.Channel, args *arguments, dates rss.DateResolver) []byte {
+	rssObject := getRssObject(yamlObject, args, dates)
 	rssXml, _ := xml.Marshal(&rssObject)
-	ioutil.WriteFile(*existingRssFilePath, rssXml, 0644)
-	fmt.Println("RSS updated successfully")
+	return rssXml
+}
+
+func getRssObject(yamlObject *yaml.Channel, args *arguments, dates rss.DateResolver) *rss.Channel {
+	rssBuilder := rss.NewBuilder(dates, args.itemsCount)
+	rssBuilder.BuildRssFromYaml(yamlObject)
+	return rssBuilder.GetRss()
 }
